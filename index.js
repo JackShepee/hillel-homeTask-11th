87a5ab421a522bd4657a327b2b068e0be3d500ba
JackShepee <salamakhaviktor@gmail.com>
2023-06-12 22:42:21 +0300

    error fixes

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function pow(num, degree) {
-  if (degree === 0) {
+  if (
+    typeof degree !== "number" ||
+    degree < 0 ||
+    Math.floor(degree) !== degree
+  ) {
+    alert("Degree must be a non-negative integer");
+    return;
+  } else if (degree === 0) {
     return 1;
   } else {
     return num * pow(num, degree - 1);
